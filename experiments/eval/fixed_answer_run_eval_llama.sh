647b244044097d90871f48c205f7275948000651
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-03-25 10:45:04 +0800

    debug

--- experiments/eval/fixed_answer_run_eval_llama.sh
+++ experiments/eval/fixed_answer_run_eval_llama.sh
@@ -1,5 +1,5 @@
-export CUDA_VISIBLE_DEVICES=1
-python evaluate_llama.py -k 5 -g 1 -d ../data/ori_mmlu_data -s ../eval_result/fixed_answerB_ori_mmlu -m meta-llama/Llama-2-7b-hf -f 1
+export CUDA_VISIBLE_DEVICES=7
+python my_evaluate_llama.py -k 5 -g 1 -d ../data/ori_mmlu_data -s ../eval_result/fixed_answerB_ori_mmlu -m meta-llama/Llama-2-7b-hf -f 1
 
 
 
