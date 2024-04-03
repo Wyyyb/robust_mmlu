51c142d7115d721d13a8e614499d324985560b7e
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-04-03 15:16:56 +0800

    add exp

--- experiments/scripts_on_darth/0404/eval_gemma_7b_mmlu.sh
+++ experiments/scripts_on_darth/0404/eval_gemma_7b_mmlu.sh
@@ -1,6 +1,6 @@
 cd ../../eval
 export CUDA_VISIBLE_DEVICES=2
-python my_evaluate_llama.py -k 5 -g 1 -d ../data/add_stemez_mmlu -s ../eval_result/0404_darth_result/ori_mmlu -m google/gemma-7b
+python my_evaluate_llama.py -k 5 -g 1 -d ../data/ori_mmlu_data -s ../eval_result/0404_darth_result/ori_mmlu -m google/gemma-7b
 
 
 
