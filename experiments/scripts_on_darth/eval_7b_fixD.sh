e642ce65894809f24033e53893a3c740814c16a1
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-04-03 12:37:25 +0800

    add exp scripts

--- experiments/scripts_on_darth/eval_7b_fixD.sh
+++
@@ -1,8 +0,0 @@
-cd ../mmlu_pro_eval
-export CUDA_VISIBLE_DEVICES=7
-python evaluate_mmlu_pro.py -k 5 -g 1 -d ../data/add_stemez_mmlu -s ../eval_result/0402_darth_result/fixD -m meta-llama/Llama-2-7b-hf -q 3
-
-
-
-
-
