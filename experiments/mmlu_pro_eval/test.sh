4c2dc8d0afa53872ef3c4768ea0d7c20f27ca226
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-04-04 23:52:34 +0800

    test hybrid

--- experiments/mmlu_pro_eval/test.sh
+++ experiments/mmlu_pro_eval/test.sh
@@ -1,8 +1,27 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-python evaluate_mmlu_pro.py -k 5 -g 8 -d ../data/add_stemez_mmlu -s ../eval_result/0404_darth_result/hybrid_ori -m mistralai/Mixtral-8x7B-v0.1
+#!/bin/bash
 
+# 设置默认参数
+ntrain=5
+ngpu=1
+data_dir="../data/merged_ori_mmlu"
+save_dir="../test_data/test_hybrid"
+options_num=4
+use_rare_symbol=False
+fixed_question_answer=-1
+scoring_method="hybrid_scoring"
+model="google/gemma-7b"
+# model="meta-llama/Llama-2-7b-hf"
 
-
-
-
-
+# cd ../../../mmlu_pro_eval/
+export CUDA_VISIBLE_DEVICES=5
+# 调用 Python 脚本，并传递参数
+python evaluate_mmlu_pro.py \
+                 --ntrain $ntrain \
+                 --ngpu $ngpu \
+                 --data_dir $data_dir \
+                 --save_dir $save_dir \
+                 --options_num $options_num \
+                 --use_rare_symbol $use_rare_symbol \
+                 --fixed_question_answer $fixed_question_answer \
+                 --scoring_method $scoring_method \
+                 --model $model
