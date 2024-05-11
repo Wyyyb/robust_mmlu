7ca01aec08c1edabf973013fb715f9a4d83c4b5d
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-05-11 13:06:14 +0800

    add prompt exps

---
+++ experiments/scripts_on_darth/schedule_0511/test_prompts/cuda_0.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# 设置默认参数
+ntrain=5
+examples_start_index=0
+prompt_type=0
+ngpu=1
+data_dir="../../data/ori_mmlu_data_json"
+save_dir="../eval_result_0511"
+scoring_method="symbol_scoring"
+# model="/ML-A100/team/mm/zhangge/Llama-2-7b-hf"
+model = "meta-llama/Llama-2-7b-hf"
+
+cd ../../../mmlu_pro_eval/
+export CUDA_VISIBLE_DEVICES=0
+
+python evaluate_mmlu_pro_0511.py \
+                 --ntrain $ntrain \
+                 --examples_start_index $examples_start_index \
+                 --prompt_type $prompt_type \
+                 --ngpu $ngpu \
+                 --data_dir $data_dir \
+                 --save_dir $save_dir \
+                 --scoring_method $scoring_method \
+                 --model $model
+
+
+prompt_type=1
+python evaluate_mmlu_pro_0511.py \
+                 --ntrain $ntrain \
+                 --examples_start_index $examples_start_index \
+                 --prompt_type $prompt_type \
+                 --ngpu $ngpu \
+                 --data_dir $data_dir \
+                 --save_dir $save_dir \
+                 --scoring_method $scoring_method \
+                 --model $model
+
+prompt_type=2
+python evaluate_mmlu_pro_0511.py \
+                 --ntrain $ntrain \
+                 --examples_start_index $examples_start_index \
+                 --prompt_type $prompt_type \
+                 --ngpu $ngpu \
+                 --data_dir $data_dir \
+                 --save_dir $save_dir \
+                 --scoring_method $scoring_method \
+                 --model $model
+
+prompt_type=3
+python evaluate_mmlu_pro_0511.py \
+                 --ntrain $ntrain \
+                 --examples_start_index $examples_start_index \
+                 --prompt_type $prompt_type \
+                 --ngpu $ngpu \
+                 --data_dir $data_dir \
+                 --save_dir $save_dir \
+                 --scoring_method $scoring_method \
+                 --model $model
+
+prompt_type=4
+python evaluate_mmlu_pro_0511.py \
+                 --ntrain $ntrain \
+                 --examples_start_index $examples_start_index \
+                 --prompt_type $prompt_type \
+                 --ngpu $ngpu \
+                 --data_dir $data_dir \
+                 --save_dir $save_dir \
+                 --scoring_method $scoring_method \
+                 --model $model
+
+prompt_type=5
+python evaluate_mmlu_pro_0511.py \
+                 --ntrain $ntrain \
+                 --examples_start_index $examples_start_index \
+                 --prompt_type $prompt_type \
+                 --ngpu $ngpu \
+                 --data_dir $data_dir \
+                 --save_dir $save_dir \
+                 --scoring_method $scoring_method \
+                 --model $model
+
+
+
