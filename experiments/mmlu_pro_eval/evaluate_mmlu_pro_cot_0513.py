753e6d2f32a33f3aecdebd2404be5138dce3de89
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-05-13 16:50:56 +0800

    update

--- experiments/mmlu_pro_eval/evaluate_mmlu_pro_cot_0513.py
+++ experiments/mmlu_pro_eval/evaluate_mmlu_pro_cot_0513.py
@@ -226,7 +226,7 @@ def eval_cot(subject, model, tokenizer, dev_df, test_df, output_path, exists_res
     print("load exists result length", len(res))
     global choices
     logging.info("evaluating " + subject)
-    batch_size = 64
+    batch_size = 128
     inference_batches = []
     label_batches = []
     in_batch_index = []
