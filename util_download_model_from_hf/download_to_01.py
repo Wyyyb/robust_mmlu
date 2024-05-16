a6805c424e6b453af746b1d0c535e545c4d8773e
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-05-16 13:34:10 +0800

    update

--- util_download_model_from_hf/download_to_01.py
+++ util_download_model_from_hf/download_to_01.py
@@ -5,7 +5,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 model_name = "Qwen/Qwen1.5-110B"
 
 # 保存模型的路径
-save_directory = "/ML-A100/team/mm/zhangge/"
+# save_directory = "/ML-A100/team/mm/zhangge/"
+save_directory = "/ML-A800/models/"
 
 # 加载模型和分词器
 model = AutoModelForCausalLM.from_pretrained(model_name)
@@ -16,6 +17,18 @@ model.save_pretrained(save_directory)
 tokenizer.save_pretrained(save_directory)
 
 
+model_name = "Qwen/Qwen1.5-72B"
 
+# 保存模型的路径
+# save_directory = "/ML-A100/team/mm/zhangge/"
+save_directory = "/ML-A800/models/"
+
+# 加载模型和分词器
+model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# 保存模型和分词器到指定路径
+model.save_pretrained(save_directory)
+tokenizer.save_pretrained(save_directory)
 
 
