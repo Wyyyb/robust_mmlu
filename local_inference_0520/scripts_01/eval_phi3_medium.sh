0b35245cc74e695679adadebd4cc22f8333cdc49
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-05-21 23:52:54 +0800

    add phi3-medium

---
+++ local_inference_0520/scripts_01/eval_phi3_medium.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ngpu=2
+save_dir="eval_results_0522/"
+global_record_file="eval_results_0522/eval_record_collection_0522.csv"
+# model="/ML-A100/team/mm/zhangge/Meta-Llama-3-70B-Instruct"
+model_list=(
+    "microsoft/Phi-3-medium-4k-instruct"
+)
+selected_subjects="all"
+gpu_util=0.8
+batch_size=2048
+
+cd ../
+export CUDA_VISIBLE_DEVICES=0,1
+
+for model in "${model_list[@]}"; do
+    python evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size
+done
+
+
+
+
