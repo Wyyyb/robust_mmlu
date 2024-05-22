a35f12cd76a4ca831d46bb7444c7ea74dabd776d
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-05-22 09:28:06 +0800

    add test

--- local_inference_0520/statistic_pred_none.py
+++ local_inference_0520/statistic_pred_none.py
@@ -1,5 +1,6 @@
 import os
 import json
+import random
 
 
 def func(input_dir):
@@ -15,13 +16,19 @@ def func(input_dir):
     print("length of data", len(data))
     pred_none_number = 0
     hit_count = 0
+    random_hit_count = 0
     for each in data:
         if not each["pred"]:
             pred_none_number += 1
             if each["answer"] == "A":
                 hit_count += 1
+            random.seed(12345)
+            x = random.randint(0, len(each["options"]) - 1)
+            if each["answer_index"] == x:
+                random_hit_count += 1
     print("pred_none_number", pred_none_number)
     print("hit_count", hit_count)
+    print("random_hit_count", random_hit_count)
 
 
 if __name__ == "__main__":
