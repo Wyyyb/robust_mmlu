960fd6de9594ca183f4534978971970795632934
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-05-30 23:04:30 +0800

    add phi

---
+++ local_inference_0520/scripts_01/eval_phi3-mini-128k.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ngpu=1
+save_dir="eval_results_0530/"
+global_record_file="eval_results_0530/eval_record_collection_0530.csv"
+# model="CohereForAI/c4ai-command-r-plus"
+model="microsoft/Phi-3-mini-128k-instruct"
+selected_subjects="all"
+gpu_util=0.8
+batch_size=2048
+
+cd ../
+export CUDA_VISIBLE_DEVICES=0
+
+python evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size
+
+
+
+
+
