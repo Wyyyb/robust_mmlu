6bc64cead2b0e21897dfaf33cbcb7e68f141ed3d
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-05-31 00:54:51 +0800

    add phi

--- local_inference_0520/scripts_01/eval_phi3_medium-128k.sh
+++ local_inference_0520/scripts_01/eval_phi3_medium-128k.sh
@@ -10,7 +10,7 @@ gpu_util=0.8
 batch_size=2048
 
 cd ../
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=0
 
 python evaluate_from_local.py \
                  --selected_subjects $selected_subjects \
