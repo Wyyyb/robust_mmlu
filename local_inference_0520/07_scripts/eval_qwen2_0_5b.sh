f0cb05a1264956d5a9370ecca3f0d8b2d58fda26
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-06 01:57:57 +0800

    add

---
+++ local_inference_0520/07_scripts/eval_qwen2_0_5b.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ngpu=1
+save_dir="eval_results_0706/"
+global_record_file="eval_results_0706/eval_record_collection_0706.csv"
+model="Qwen/Qwen2-0.5B-Instruct"
+selected_subjects="all"
+gpu_util=0.8
+batch_size=2048
+
+cd ../
+export CUDA_VISIBLE_DEVICES=4
+export HF_HOME=/mnt/tjena/yubo/hf_home
+
+python evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size
+
+
+# model="/ML-A800/models/gemma-2-9b-it"
+model="Qwen/Qwen2-0.5B"
+
+python evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size
+
