fa681673acc8fd108718a32ba9325d05c966d67c
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-30 02:14:40 +0800

    add

--- local_inference_0520/scripts_0719/eval_llama-3_1-70b.sh
+++ local_inference_0520/scripts_0719/eval_llama-3_1-70b.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-ngpu=8
+ngpu=4
 save_dir="eval_results_0729/"
 global_record_file="eval_results_0729/eval_record_collection_0729.csv"
 model="/mnt/tjena/yubo/models/Meta-Llama-3.1-70B"
 # model="meta-llama/Meta-Llama-3-8B"
 # model="/ML-A800/models/Meta-Llama-3-8B"
 selected_subjects="all"
-gpu_util=0.8
+gpu_util=0.85
 batch_size=2048
 dataset="mmlu-pro"
 
 cd ../
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=2,3,5,6
 
 python evaluate_from_local.py \
                  --selected_subjects $selected_subjects \
@@ -25,6 +25,17 @@ python evaluate_from_local.py \
                  --dataset $dataset
 
 
+model="/mnt/tjena/yubo/models/Meta-Llama-3.1-70B-Instruct"
+
+python evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size \
+                 --dataset $dataset
 
 
 
