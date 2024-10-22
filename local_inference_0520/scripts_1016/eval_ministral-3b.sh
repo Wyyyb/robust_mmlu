4d3fde72eb1b1aee9d4ee835138316967745de53
Wyyyb <yubo.wang.sunny@gmail.com>
2024-10-22 11:30:52 +0800

    add

--- local_inference_0520/scripts_1016/eval_ministral-3b.sh
+++ local_inference_0520/scripts_1016/eval_ministral-3b.sh
@@ -4,8 +4,8 @@
 ngpu=4
 save_dir="eval_results_1022/"
 global_record_file="eval_results_1022/eval_record_collection_1022.csv"
-# model="/data/yubowang/models/mistral-3b-instruct"
-model="ministral/Ministral-3b-instruct"
+model="/data/yubowang/models/mistral-3b-instruct"
+# model="ministral/Ministral-3b-instruct"
 selected_subjects="all"
 gpu_util=0.8
 batch_size=8
@@ -14,7 +14,8 @@ dataset="mmlu-pro"
 cd ../
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-python evaluate_from_local_1017.py \
+# python evaluate_from_local_1017.py \
+python evaluate_from_local_0907.py \
                  --selected_subjects $selected_subjects \
                  --ngpu $ngpu \
                  --save_dir $save_dir \
