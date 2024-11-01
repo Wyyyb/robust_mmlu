f2dc1404724de526fa0e33d300b944de01e0fe61
Wyyyb <yubo.wang.sunny@gmail.com>
2024-11-01 11:53:06 +0800

    add

--- local_inference_0520/download_hf.sh
+++ local_inference_0520/download_hf.sh
@@ -13,3 +13,9 @@ huggingface-cli download --repo-type model Qwen/Qwen2.5-7B --local-dir /gpfs/pub
 huggingface-cli download --repo-type model ministral/Ministral-3b-instruct --local-dir /data/yubowang/models/Ministral-3b-instruct
 
 huggingface-cli download --repo-type model mistralai/Mistral-Small-Instruct-2409 --local-dir /data/yubowang/models/Mistral-Small-Instruct-2409
+
+huggingface-cli download --repo-type model nvidia/NVLM-D-72B --local-dir /gpfs/public/research/xy/yubowang/models/NVLM-D-72B
+
+huggingface-cli download --repo-type model Qwen/Qwen2.5-3B --local-dir /gpfs/public/research/xy/yubowang/models/Qwen2.5-3B
+
+
---
+++ local_inference_0520/scripts_1101/eval_granite-3.0-8b-base.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#source /gpfs/public/research/miniconda3/bin/activate
+#conda activate vllm
+ngpu=1
+save_dir="eval_results_1101/"
+global_record_file="eval_results_1101/eval_record_collection_1101.csv"
+model="/data/yubowang/models/granite-3.0-8b-base"
+selected_subjects="all"
+gpu_util=0.8
+batch_size=100
+dataset="mmlu-pro"
+export HF_HOME="/data/yubowang/hf_home"
+
+cd ../
+export CUDA_VISIBLE_DEVICES=4
+
+python evaluate_from_local_0907.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size \
+                 --dataset $dataset
+
+
+
+
+
