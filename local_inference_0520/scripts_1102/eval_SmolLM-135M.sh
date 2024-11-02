533d1b374105ec0b599fa7284779af2a5188a7da
Wyyyb <yubo.wang.sunny@gmail.com>
2024-11-02 10:21:46 +0800

    add

---
+++ local_inference_0520/scripts_1102/eval_SmolLM-135M.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#source /gpfs/public/research/miniconda3/bin/activate
+#conda activate vllm
+ngpu=1
+save_dir="eval_results_1102/"
+global_record_file="eval_results_1102/eval_record_collection_1102.csv"
+model="/data/yubowang/models/SmolLM2-135M"
+selected_subjects="all"
+gpu_util=0.8
+batch_size=100
+dataset="mmlu-pro"
+export HF_HOME="/data/yubowang/hf_home"
+export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
+
+cd ../
+export CUDA_VISIBLE_DEVICES=0
+
+python evaluate_from_local_0907.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size \
+                 --dataset $dataset
+
+
+
+
+
