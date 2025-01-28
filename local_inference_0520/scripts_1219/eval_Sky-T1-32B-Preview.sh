4f24350253e6fe2e6171f05188eb0289ad3b8311
Wyyyb <yubo.wang.sunny@gmail.com>
2025-01-28 23:04:05 +0800

    add

--- local_inference_0520/scripts_1219/eval_Sky-T1-32B-Preview.sh
+++ local_inference_0520/scripts_1219/eval_Sky-T1-32B-Preview.sh
@@ -4,7 +4,7 @@ conda activate mmlu-pro
 ngpu=4
 save_dir="eval_results_0127/"
 global_record_file="eval_results_0127/eval_record_collection_0127.csv"
-model="/gpfs/public/research/xy/yubowang/models/Sky-T1-32B-Preview"
+model="/gpfs/public/research/xy/yubowang/models/Qwen2.5-32B-Instruct"
 selected_subjects="all"
 gpu_util=0.95
 batch_size=100
